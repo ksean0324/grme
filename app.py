62ff3e046e15146bb305c3aae72c9fbaddc0d5e9
SEAN <ksean0324@gmail.com>
2026-01-21 20:43:52 +0900

    auto update

--- app.py
+++ app.py
@@ -195,9 +195,9 @@ def admin():
     """
 
 # =====================
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+import os
+
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000))
+)
